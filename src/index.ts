30e08fea1f3d339187c9b903df459765596a1f7f
Zenith Wogwugwu <uzenith360@gmail.com>
2023-02-04 20:28:14 +0100

    feat: add httpstatus enum

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { exponentialBackoff } from "./exponential-backoff";
 import { HttpError } from "./http-error";
 import { httpRetry } from "./http-retry";
+import httpStatus from "./http-status.enum";
 
 export {
     HttpError,
     httpRetry,
+    httpStatus,
     exponentialBackoff,
 };
