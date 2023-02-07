f48d27d194df30399db419cc507919eefed09a3e
Zenith Wogwugwu <uzenith360@gmail.com>
2023-02-07 13:02:35 +0100

    feat: add PaginatedResult

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import { exponentialBackoff } from "./exponential-backoff";
 import { HttpError } from "./http-error";
 import { httpRetry } from "./http-retry";
 import httpStatus from "./http-status.enum";
+import PaginatedResult from "./paginated-result.interface";
 
 export {
     HttpError,
     httpRetry,
     httpStatus,
+    PaginatedResult,
     exponentialBackoff,
 };
